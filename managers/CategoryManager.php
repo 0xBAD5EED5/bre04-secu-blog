ac8de6dcf9c19b412acd9ced69966af613d6e270
0xBAD5EED5 <eyestech404@protonmail.com>
2025-06-30 10:02:35 +0000

    feat: Ajouter les méthodes findAll et findOne pour gérer les catégories

--- managers/CategoryManager.php
+++ managers/CategoryManager.php
@@ -1,5 +1,30 @@
 <?php
 class CategoryManager extends AbstractManager
 {
+    // Récupérer toutes les catégories
+    public function findAll(): array
+    {
+        $stmt = $this->db->query('SELECT * FROM categories');
+        $categories = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $category = new Category($row['title'], $row['description']);
+            $category->setId($row['id']);
+            $categories[] = $category;
+        }
+        return $categories;
+    }
 
+    // Récupérer une catégorie par son id
+    public function findOne(int $id): ?Category
+    {
+        $stmt = $this->db->prepare('SELECT * FROM categories WHERE id = :id');
+        $stmt->execute(['id' => $id]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($row) {
+            $category = new Category($row['title'], $row['description']);
+            $category->setId($row['id']);
+            return $category;
+        }
+        return null;
+    }
 }
