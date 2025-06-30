1f83bbe5ba9f706cdbc129af6992600760c445e0
Talbi Said <eyestech404@protonmail.com>
2025-06-30 09:36:49 +0000

    feat: Completing Step 3

--- models/Comment.php
+++ models/Comment.php
@@ -1,11 +1,52 @@
 <?php
-/**
- * @author : Gaellan
- * @link : https://github.com/Gaellan
- */
-
 
 class Comment
 {
+    private ?int $id = null;
 
+    public function __construct(
+        private string $content,
+        private int $user_id,
+        private int $post_id
+    ) {}
+
+    // ID
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    // Content
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+    public function setContent(string $content): void
+    {
+        $this->content = content;
+    }
+
+    // User_id
+    public function getUser_id(): int
+    {
+        return $this->user_id;
+    }
+    public function setUser_id(int $user_id): void
+    {
+        $this->user_id = $user_id;
+    }
+
+    // Post_id
+    public function getPost_id(): int
+    {
+        return $this->post_id;
+    }
+    public function setPost_id(int $post_id): void
+    {
+        $this->post_id = $post_id;
+    }
 }
